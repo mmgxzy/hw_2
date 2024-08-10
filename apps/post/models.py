02ad5a380ebaec52754054e96aca811f5d221dd4
mmgxzy <kyzjibek@gmail.com>
2024-08-10 16:37:18 +0600

    hw2

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    fullname = models.CharField(
+        max_length=155
+    )
+    age = models.IntegerField(
+        default = 0
+    )
+    hobby = models.TextField(
+        
+        blank = True, null = True
+    )
+
